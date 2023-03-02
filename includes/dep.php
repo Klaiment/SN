071ec66b26f73d0dcd73eb82366f0becb8ca0598
Klaiment <clement62ing@gmail.com>
2023-03-02 19:51:21 +0100

    v2

--- includes/dep.php
+++ includes/dep.php
@@ -1,10 +1,26 @@
 <?php
 session_start();
+if (isset($_SESSION['id']) && !empty($_SESSION['id'])) {
+    //Compte connecté
+/*    echo $_SESSION['id']."<br>";
+    echo $_SESSION['pseudo']."<br>";
+    echo $_SESSION['email']."<br>";
+    echo $_SESSION['avatar']."<br>";*/
+} else {
+    //On renvoie vers la page de login
+    if ($_SERVER['REQUEST_URI'] == "/login.php" || $_SERVER['REQUEST_URI'] == "/register.php") {
+    }else{
+        header('Location: login.php');
+    }
+
+}
 
 ?>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <script src="https://cdn.tailwindcss.com"></script>
+<script src="https://flowbite.com/docs/flowbite.min.js"></script>
+<script src="https://flowbite.com/docs/datepicker.min.js"></script>
 <link rel="icon" href="/assets/favicon2.PNG">
 <script src="/dist/toasteur.min.js"></script>
 <link rel="stylesheet" href="/dist/themes/toasteur-default.min.css">
